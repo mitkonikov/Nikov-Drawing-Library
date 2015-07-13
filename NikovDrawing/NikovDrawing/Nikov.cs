ebc6dbf4f1c1fd17e2a1bc0b2a9b9a2f89831521
Mitko Nikov <mitkonikov01@gmail.com>
2015-07-13 12:07:28 +0200

    initial commit

---
+++ NikovDrawing/NikovDrawing/Nikov.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NikovDrawing
+{
+    // This is open source Library created by Mitko Nikov
+
+    // GitHub: 
+
+    class Trigonometry
+    {
+        // This class takes care of the basics Drawing Algorithms using Trigonometry
+    }
+}
